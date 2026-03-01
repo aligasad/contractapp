09a9319e61ba2d9616624b2e2d72faf7a18213a2
aligasad <asadalam4291@gmail.com>
2026-03-02 00:32:03 +0530

    change

--- src/components/protector/ProtectedRoute.jsx
+++ src/components/protector/ProtectedRoute.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import Loader from "../loader/Loader.jsx";
-import { auth } from "../../firebase/FirebaseConfig.jsx";
 import { useAuth } from "./AuthContext.jsx";
 
 function ProtectedRoute({ children }) {
